19b6307730eecf47af4e81b39eba48bacb76853a
farisawan-2000 <farisawan.2000@gmail.com>
2021-01-17 19:52:45 -0500

    updated progress

--- public/index.js
+++ public/index.js
@@ -2,18 +2,22 @@
 // console.log(ref) // me when i leak my database!!!!!
 // console.log(firebase)
 
-const firebaseConfig = {
-  apiKey: "AIzaSyBr3jC-jwmknhJJNS6Py8IRRPQhVE3zkdk",
-  authDomain: "kirby64progress.firebaseapp.com",
-  databaseURL: "https://kirby64progress-default-rtdb.firebaseio.com",
-  projectId: "kirby64progress",
-  storageBucket: "kirby64progress.appspot.com",
-  messagingSenderId: "3532178605",
-  appId: "1:3532178605:web:8ff58e74396778f008f224",
-  measurementId: "G-Q6KHNQ6XM3"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBr3jC-jwmknhJJNS6Py8IRRPQhVE3zkdk",
+//   authDomain: "kirby64progress.firebaseapp.com",
+//   databaseURL: "https://kirby64progress-default-rtdb.firebaseio.com",
+//   projectId: "kirby64progress",
+//   storageBucket: "kirby64progress.appspot.com",
+//   messagingSenderId: "3532178605",
+//   appId: "1:3532178605:web:8ff58e74396778f008f224",
+//   measurementId: "G-Q6KHNQ6XM3"
+// };
 
-firebase.initializeApp(firebaseConfig);
+// firebase.initializeApp(firebaseConfig);
 
+console.log("AAAAAAAAAAAAAA")
+var ref = firebase.database().ref();
 
-console.log(firebase.database().ref())
+ref.on("value", function(snapshot) {
+   console.log(snapshot.val());
+})
